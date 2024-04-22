6b5f3259fcaf0ab3f513acb1bb5b112ee588f793
Daniel Mesejo <mesejoleon@gmail.com>
2024-04-22 15:19:10 +0200

    chore: cleaning github actions

--- python/koenigsberg/__init__.py
+++ python/koenigsberg/__init__.py
@@ -1,5 +1,12 @@
 from .main import Backend
 
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
+
 ba = Backend()
 
 
